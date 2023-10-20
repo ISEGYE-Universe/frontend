2c37f4630f1c3a67e75e08d43d1f8fba7bb504b3
stripy1026 <aequatio1026@gmail.com>
2023-10-20 22:20:09 +0900

    feat: :poop: temporary scroll for news thumnail list

--- src/components/News/NewsThumnailList.tsx
+++ src/components/News/NewsThumnailList.tsx
@@ -1,24 +1,34 @@
 import { NewsThumnail } from './NewsThumnail'
 
+const DUMMY_THUMNAIL_LIST = [
+  {
+    id: 1,
+    dummyString: 'Thumnail Inner 1',
+  },
+  {
+    id: 2,
+    dummyString: 'Thumnail Inner 2',
+  },
+  {
+    id: 3,
+    dummyString: 'Thumnail Inner 3',
+  },
+]
+
 export const NewsThumnailList = () => {
   return (
     <div
       css={{
         display: 'flex',
         gap: 10,
-        justifyContent: 'center',
-        overflow: 'hidden',
+        overflowX: 'scroll',
       }}
     >
-      <NewsThumnail>
-        <span>Thumnail Inner content</span>
-      </NewsThumnail>
-      <NewsThumnail>
-        <span>Thumnail Inner content</span>
-      </NewsThumnail>
-      <NewsThumnail>
-        <span>Thumnail Inner content</span>
-      </NewsThumnail>
+      {DUMMY_THUMNAIL_LIST.map((thumnail) => (
+        <NewsThumnail key={thumnail.id}>
+          <span>{thumnail.dummyString}</span>
+        </NewsThumnail>
+      ))}
     </div>
   )
 }
