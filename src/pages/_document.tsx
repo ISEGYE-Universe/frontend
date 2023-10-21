a6510a86b165d2ee806d5c70abf3bd8280bb58fd
rnintai <ho46825@gmail.com>
2023-10-21 17:24:58 +0900

    Feat: 기본 Font style 정의
    - _document.tsx에 GlobalStyle 적용
    - 네이밍: Figma 폰트 가이드의 이름을 PascalCase로 변환 후, 각 폰트 크기 별로 Light/Regular/Bold를 뒤에 붙여주었습니다.
    e.g. text_md -> TextMdLight, TextMdRegular, TextMdBold

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,11 @@
+import GlobalStyle from '@/styles/Global'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <GlobalStyle />
       <body>
         <Main />
         <NextScript />
