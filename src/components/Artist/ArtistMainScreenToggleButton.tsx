850ffef208a5017eed9c651e6494c34df1231dd7
stripy1026 <aequatio1026@gmail.com>
2023-10-23 00:40:06 +0900

    feat: :sparkles: highlight colors when twitch is on

--- src/components/Artist/ArtistMainScreenToggleButton.tsx
+++ src/components/Artist/ArtistMainScreenToggleButton.tsx
@@ -6,6 +6,19 @@ export const ArtistMainScreenToggleButton = ({
   twitchNickname,
   isBangOn,
 }: IsedolTwitchInfoType) => {
+  const personalColor =
+    id === 'Ine'
+      ? color.IneViolet
+      : id === 'Jingburger'
+      ? color.JingburgerYellow
+      : id === 'Lilpa'
+      ? color.LilpaNavy
+      : id === 'Jururu'
+      ? color.JururuPink
+      : id === 'Gosegu'
+      ? color.GoseguSky
+      : color.VIichanGreen
+
   return (
     <div css={{ display: 'flex', flexDirection: 'row-reverse', gap: 20 }}>
       <div // later using ISD profile common component
@@ -23,8 +36,20 @@ export const ArtistMainScreenToggleButton = ({
           gap: 10,
         }}
       >
-        <span>{twitchNickname}</span>
-        <span>{id}</span>
+        <span
+          css={{
+            color: isBangOn ? '#151515' : '#909090',
+          }}
+        >
+          {twitchNickname}
+        </span>
+        <span
+          css={{
+            color: isBangOn ? personalColor : '#909090',
+          }}
+        >
+          {id}
+        </span>
       </div>
     </div>
   )
