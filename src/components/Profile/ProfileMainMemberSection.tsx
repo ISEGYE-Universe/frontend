cf501f3a6e25c61da6904d6ff282032a3bf996f2
rnintai <ho46825@gmail.com>
2023-10-22 13:41:23 +0900

    Feat: Profile 메인 페이지 뒷배경 반응형 구현
    - svg viewbox 및 preserveAspectRatio 속성 이용
    ## ref
    - https://tecoble.techcourse.co.kr/post/2021-10-24-svg-viewBox/
    - https://jundol.me/51

--- src/components/Profile/ProfileMainMemberSection.tsx
+++ src/components/Profile/ProfileMainMemberSection.tsx
@@ -9,6 +9,15 @@ interface ProfileMainMemberSectionProps {
   height: number
 }
 
+/**
+ * @멤버소개 멤버 별 마우스 over가능한 섹션
+ * @param memberIndex 배경 이미지 내 멤버 인덱스
+ * @param x 마우스 액션 구역의 x좌표
+ * @param y 마우스 액션 구역의 y좌표
+ * @param width 마우스 액션 구역의 width
+ * @param height 마우스 액션 구역의 height
+ * @author 민타이
+ */
 const ProfileMainMemberSection = ({
   memberIndex,
   x,
