3f5faa75ce7034d783c37d813c3dd8eabc6dbf00
rnintai <ho46825@gmail.com>
2023-10-24 20:54:25 +0900

    fix: :bug: 배경 이미지 위치 보정 작업

--- src/components/Profile/ProfileMainBg.tsx
+++ src/components/Profile/ProfileMainBg.tsx
@@ -88,7 +88,7 @@ const ProfileMainBg = () => {
         <svg
           version="1.1"
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1834 955"
+          viewBox="0 -0.5 1920 1000"
           preserveAspectRatio="xMidYMid slice"
           width="100%"
           height="100%"
