f49d1cedb97e7901baa2376a5554bc0f794b51d2
stripy1026 <aequatio1026@gmail.com>
2023-10-25 00:22:42 +0900

    feat: :sparkles: import artist main screen images

--- src/components/Artist/ArtistMainScreen.tsx
+++ src/components/Artist/ArtistMainScreen.tsx
@@ -1,5 +1,14 @@
 import { color } from '../News/color'
 
+const SCREEN_MAP = {
+  Ine: require('assets/main-artist/이네땅_바탕화면_1.png'),
+  Jingburger: require('assets/main-artist/버거땅_바탕화면_1.png'),
+  Lilpa: require('assets/main-artist/릴파땅_바탕화면_1.png'),
+  Jururu: require('assets/main-artist/르르땅_바탕화면_1.png'),
+  Gosegu: require('assets/main-artist/세구땅_바탕화면_1.png'),
+  VIichan: require('assets/main-artist/챠니_바탕화면_1.png'),
+}
+
 type ArtistMainScreenProps = {
   highlightedMember: string
 }
