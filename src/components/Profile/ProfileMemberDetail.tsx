33f0e97516835542bffe57c2b4d8c53dac9964be
rnintai <ho46825@gmail.com>
2023-10-25 23:52:06 +0900

    feat: ✨ 멤버소개 개별 페이지 로드 시 transition 추가
    framer-motion 라이브러리 활용

--- src/components/Profile/ProfileMemberDetail.tsx
+++ src/components/Profile/ProfileMemberDetail.tsx
@@ -2,6 +2,7 @@ import ProfileIntroductionBox from '@/components/Profile/ProfileIntroductionBox'
 import { css } from '@emotion/react'
 import { useState } from 'react'
 import profileData from '@/data/profile.json'
+import TransitionLayout from '@/components/TransitionLayout/TransitionLayout'
 
 interface MemberIntroduction {
   mainTitle: string
@@ -37,90 +38,92 @@ const ProfileMemberDetail = ({ data }: ProfileMemberDetailProps) => {
   `
 
   return (
-    <div
-      css={css`
-        width: 100%;
-        height: 100vh;
-        overflow-y: hidden;
-      `}
-    >
-      {/* 이미지 슬라이더 */}
-      {data.galleryImageURL.map((img, i) => (
-        <img
-          src={img}
-          alt={`gallery-${i + 1}`}
-          key={`gallery-${i}`}
+    <TransitionLayout duration={0.5}>
+      <div
+        css={css`
+          width: 100%;
+          height: 100vh;
+          overflow-y: hidden;
+        `}
+      >
+        {/* 이미지 슬라이더 */}
+        {data.galleryImageURL.map((img, i) => (
+          <img
+            src={img}
+            alt={`gallery-${i + 1}`}
+            key={`gallery-${i}`}
+            css={[
+              slideImageStyle,
+              css`
+                opacity: ${pageIndex === i ? 1 : 0};
+              `,
+            ]}
+          />
+        ))}
+
+        {/* background */}
+        <a href="/profile">
+          <img
+            src={profileData.image.memberGalleryBg}
+            alt=""
+            css={css`
+              width: 100%;
+              height: 100%;
+              object-fit: cover;
+              position: absolute;
+              opacity: 0.9;
+            `}
+          />
+        </a>
+
+        {/* 좌우 버튼 */}
+        <button
           css={[
-            slideImageStyle,
+            navButtonStyle,
             css`
-              opacity: ${pageIndex === i ? 1 : 0};
+              left: 130px;
             `,
           ]}
-        />
-      ))}
+          onClick={() => {
+            if (pageIndex === 0) {
+              setPageIndex(galleryLen - 1)
+            } else {
+              setPageIndex(pageIndex - 1)
+            }
+          }}
+        >
+          <img src="/images/icon/left-chevron.svg" css={iconStyle}></img>
+        </button>
+        <button
+          css={[
+            navButtonStyle,
+            css`
+              right: 130px;
+            `,
+          ]}
+          onClick={() => {
+            if (pageIndex === galleryLen - 1) {
+              setPageIndex(0)
+            } else {
+              setPageIndex(pageIndex + 1)
+            }
+          }}
+        >
+          <img src="/images/icon/right-chevron.svg" css={iconStyle}></img>
+        </button>
 
-      {/* background */}
-      <a href="/profile">
-        <img
-          src={profileData.image.memberGalleryBg}
-          alt=""
-          css={css`
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-            position: absolute;
-            opacity: 0.9;
-          `}
-        />
-      </a>
-
-      {/* 좌우 버튼 */}
-      <button
-        css={[
-          navButtonStyle,
-          css`
-            left: 130px;
-          `,
-        ]}
-        onClick={() => {
-          if (pageIndex === 0) {
-            setPageIndex(galleryLen - 1)
-          } else {
-            setPageIndex(pageIndex - 1)
-          }
-        }}
-      >
-        <img src="/images/icon/left-chevron.svg" css={iconStyle}></img>
-      </button>
-      <button
-        css={[
-          navButtonStyle,
-          css`
-            right: 130px;
-          `,
-        ]}
-        onClick={() => {
-          if (pageIndex === galleryLen - 1) {
-            setPageIndex(0)
-          } else {
-            setPageIndex(pageIndex + 1)
-          }
-        }}
-      >
-        <img src="/images/icon/right-chevron.svg" css={iconStyle}></img>
-      </button>
-
-      {/* 소개 */}
-      <ProfileIntroductionBox
-        mainTitle={data.mainTitle}
-        isShowSns
-        ytLink={data.socialLink.ytLink}
-        igLink={data.socialLink.igLink}
-        twLink={data.socialLink.twLink}
-        description={data.description}
-        margin={'0 0 168px 150px'}
-      ></ProfileIntroductionBox>
-    </div>
+        {/* 소개 */}
+        <ProfileIntroductionBox
+          mainTitle={data.mainTitle}
+          isShowSns
+          ytLink={data.socialLink.ytLink}
+          igLink={data.socialLink.igLink}
+          twLink={data.socialLink.twLink}
+          description={data.description}
+          margin={'0 0 168px 150px'}
+        ></ProfileIntroductionBox>
+      </div>
+    </TransitionLayout>
   )
 }
 
