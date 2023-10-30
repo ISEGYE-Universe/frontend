0795d3a567cd7ce32080a8a8c045f34183d99f6b
rnintai <ho46825@gmail.com>
2023-10-30 23:32:58 +0900

    feat: :sparkles: 상단 광고 배너 마크업

---
+++ src/components/Chart/ArticleADBanner.tsx
@@ -0,0 +1,79 @@
+import { TextSmLight, TitleMdBold, TitleSmLight } from '@/styles/Font'
+import { css } from '@emotion/react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+interface ArticleADBannerProps {
+  title: string
+  desc?: string
+  CTAText?: string
+  bgImage?: string
+  bgColor?: string
+}
+
+const contentsContainerStyle = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+const textContainerStyle = css`
+  display: flex;
+  flex-direction: column;
+`
+const CTAContainerStyle = css`
+  display: flex;
+  align-items: center;
+  gap: 11px;
+`
+
+const ArticleADBanner = ({
+  title,
+  desc,
+  CTAText,
+  bgImage,
+  bgColor,
+}: ArticleADBannerProps) => {
+  const mainContainerStyle = css`
+    width: 100%;
+    border-radius: 10px;
+    color: #fff;
+    padding: 20px 40px 26px;
+    background:
+      linear-gradient(90deg, ${bgColor} 37.85%, rgba(234, 68, 118, 0.5) 100%),
+      url('${bgImage}'),
+      lightgray 50% / cover no-repeat;
+    background-position: center;
+  `
+  return (
+    <Link href="#">
+      <section css={mainContainerStyle}>
+        <div css={contentsContainerStyle}>
+          <div css={textContainerStyle}>
+            <h1 css={TitleMdBold}>{title}</h1>
+            <h2
+              css={[
+                TextSmLight,
+                css`
+                  font-weight: 100;
+                `,
+              ]}
+            >
+              {desc}
+            </h2>
+          </div>
+          <div css={CTAContainerStyle}>
+            <span css={TitleSmLight}>{CTAText}</span>
+            <Image
+              src={'/images/icon/right-chevron.svg'}
+              alt="right icon"
+              width={12}
+              height={24}
+            ></Image>
+          </div>
+        </div>
+      </section>
+    </Link>
+  )
+}
+
+export default ArticleADBanner
---
+++ src/pages/chart/index.tsx
@@ -0,0 +1,30 @@
+import ArticleADBanner from '@/components/Chart/ArticleADBanner'
+import TempLayout from '@/components/TempLayout/TempLayout'
+import { css } from '@emotion/react'
+
+const articleStyle = css`
+  width: 1060px;
+  margin: 0 auto;
+  padding: 44px 0 141px 0;
+`
+
+const Chart = () => {
+  return (
+    <TempLayout>
+      <article css={articleStyle}>
+        {/* 배너 */}
+        <ArticleADBanner
+          title="멜론 2022 MMA 내돌내상 2023"
+          desc="시상수 TOP 10 달성시 실물 상패가 수여되고 MMA 메인페이지에 소개됩니다."
+          CTAText="참여하러 가기"
+          bgImage="https://s3-alpha-sig.figma.com/img/8d82/37bd/d28c3ce5c72d6035ef3cdf9c9d136691?Expires=1699833600&Signature=HqpNmXXiQt4pleTGrBknaB~RtS5TeuwpQzinF-FbQlGanVxTDsWn2eQ2yJLN~o~2a8k7cIF5lNDdmFH49sqj7J6L0R79x2TvIoNlM1xPowW-09Lznvvl8Xs1Zafh8h3-qaM4L3CB-caWPKMrHPHfTrKNEIca~jh8jg85LdidO4s2G~Uy6qSObLb~kj97vWRhgRtMguHMNPFijSJ1cvcwLp3MuCXUhvj~7rfEkLyuVzdttLxy2SlOTe0ZNi9GHEewyFVGPL0Qd0j3bWVi5skrbofVRQepxcnmca5wrEtHAT6GNgSrU~rUL9tk7gKau0fqzYopHFTncZ79vVd-AJ2pjQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+          bgColor="#EA4476"
+        ></ArticleADBanner>
+        {/* 차트정보 */}
+        {/* 역대기록 */}
+      </article>
+    </TempLayout>
+  )
+}
+
+export default Chart
