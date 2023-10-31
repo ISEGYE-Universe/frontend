fc1691e31cfa7a19c42a3102c66880754a448ffc
rnintai <ho46825@gmail.com>
2023-11-01 00:14:34 +0900

    feat: :sparkles: HRLine margin prop 추가

--- src/components/Chart/HRLine.tsx
+++ src/components/Chart/HRLine.tsx
@@ -1,14 +1,19 @@
 import { css } from '@emotion/react'
 
-const hrLineStyle = css`
-  margin: 30px 0;
-  border: 0;
-  height: 1px;
-  background-color: #d9d9d9;
-`
-
-const HRLine = () => {
-  return <hr css={hrLineStyle} />
+interface HRLineProps {
+  margin?: string
+}
+const HRLine = ({ margin }: HRLineProps) => {
+  return (
+    <hr
+      css={css`
+        margin: ${margin || '30px 0'};
+        border: 0;
+        height: 1px;
+        background-color: #d9d9d9;
+      `}
+    />
+  )
 }
 
 export default HRLine
