bc474ad19cc7ad99104b3fca1967a3724ca3d302
rnintai <ho46825@gmail.com>
2023-10-31 23:02:50 +0900

    feat: :sparkles: Hot100 데이터 json파일에 정의 및 반복문 적용

--- src/components/Chart/ChartLeftSection.tsx
+++ src/components/Chart/ChartLeftSection.tsx
@@ -2,28 +2,50 @@ import { css } from '@emotion/react'
 import ChartMusicCard from './ChartMusicCard'
 import SubTitle from './SubTitle'
 import Title from './Title'
+import chartData from '@/data/chart.json'
 
 const titleContainer = css`
   margin-bottom: 22px;
 `
+const hot100Container = css`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`
+
+const hrLineStyle = css`
+  margin: 30px 0;
+  border: 0;
+  height: 1px;
+  background-color: #d9d9d9;
+`
+
 const ChartLeftSection = () => {
   return (
     <>
       {/* 멜론 HOT 100 */}
       <section>
         <div css={titleContainer}>
+          {/* 추후 컴포넌트화를 통해 기준 시간을 prop으로 리팩토링 할 예정 */}
           <SubTitle>오늘 23:00 기준</SubTitle>
           <Title>멜론 HOT 100</Title>
         </div>
 
-        <ChartMusicCard
-          title="KIDDING"
-          position={18}
-          change={1}
-          artist="이세계아이돌"
-          thumb="https://s3-alpha-sig.figma.com/img/11cf/1bba/400a79b31a645001a270e66d4bd85bcf?Expires=1699833600&Signature=XHq4UyJ3caQ6BRLtJKwmBoeAEhFak8Jnnj0nDZ6tDq3uoyxVxf8nR84fJ41TT2poUxbeTgyLClPbpNHVPAk5WNFy3cvZZSfbf0EO66BeZ2wkpyJPM5wts1QUrHTlu-Xm9kbFyif6froCe3FkMLCtWVrI6tW~SrL2Zhpkk4i5ZXe8Tau2h1n5iIjo5r1ID8fbZ-DJkyZ8pYlR0EkA6aMDDGSrXgaOtdVYdBsV1ZtPwyArGsGG5XU3VLeBHpEXmul-7r5DOnKObMHveSej7V4D3wmfsh1ZAfbYzGVq6gWLY3~cbI7ecrDy-1clL-NW7z2LkJ2bJ9u9~UESkKkmYc6ayg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-        ></ChartMusicCard>
+        <ul css={hot100Container}>
+          {chartData.hot100List.map((m) => (
+            <ChartMusicCard
+              key={`hot100-card-${m.title}`}
+              title={m.title}
+              position={m.position}
+              change={m.change}
+              artist={m.artist}
+              thumb={m.thumb}
+            ></ChartMusicCard>
+          ))}
+        </ul>
       </section>
+      {/* 구분선 */}
+      <hr css={hrLineStyle} />
       {/* 실시간 차트 */}
       {/* 음반 관련 소식 */}
     </>
