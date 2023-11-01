736c9dd2ab8677c34ee00c67c2eaaede7b46f01c
spde3289 <spde3289@naver.com>
2023-11-01 12:35:13 +0900

    feat: 제목태그 reset css

--- src/styles/Global.tsx
+++ src/styles/Global.tsx
@@ -11,6 +11,15 @@ const style = css`
     padding: 0;
   }
 
+  h1, 
+  h2, 
+  h3, 
+  h4, 
+  h5, 
+  h6{
+    margin: 0;
+  }
+
   @font-face {
     font-family: 'NotoSansCJKkr';
     font-weight: 100;
