13314de031e68569ff02d0117c5656c21179b083
stripy1026 <aequatio1026@gmail.com>
2023-11-03 23:31:13 +0900

    feat: :sparkles: news main banner scroll button markup

--- src/components/News/NewsThumnailList.tsx
+++ src/components/News/NewsThumnailList.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { NewsThumnail } from './NewsThumnail'
 
 const DUMMY_THUMNAIL_LIST = [
@@ -19,16 +20,69 @@ export const NewsThumnailList = () => {
   return (
     <div
       css={{
-        display: 'flex',
-        gap: 10,
-        overflowX: 'scroll', // temporary
+        position: 'relative',
       }}
     >
-      {DUMMY_THUMNAIL_LIST.map((thumnail) => (
-        <NewsThumnail key={thumnail.id}>
-          <span>{thumnail.dummyString}</span>
-        </NewsThumnail>
-      ))}
+      <div
+        css={{
+          position: 'absolute',
+          width: 1300,
+          height: 500,
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          paddingLeft: 60,
+          paddingRight: 60,
+        }}
+      >
+        <button
+          css={{
+            width: 50,
+            height: 50,
+            backgroundColor: '#252525',
+            borderRadius: 50,
+          }}
+        >
+          <Image
+            src="images/icon/banner-button-left-arrow.svg"
+            alt="left-arrow"
+            width={14}
+            height={24}
+          />
+        </button>
+        <button
+          css={{
+            width: 50,
+            height: 50,
+            backgroundColor: '#252525',
+            borderRadius: 50,
+          }}
+        >
+          <Image
+            src="images/icon/banner-button-right-arrow.svg"
+            alt="right-arrow"
+            width={14}
+            height={24}
+          />
+        </button>
+      </div>
+      <div
+        css={{
+          display: 'flex',
+          gap: 10,
+          overflowX: 'hidden',
+          // animation with button
+          // justifyContent: 'flex-start',
+          // justifyContent: 'center',
+          // justifyContent: 'flex-end',
+        }}
+      >
+        {DUMMY_THUMNAIL_LIST.map((thumnail) => (
+          <NewsThumnail key={thumnail.id}>
+            <span>{thumnail.dummyString}</span>
+          </NewsThumnail>
+        ))}
+      </div>
     </div>
   )
 }
