5a228a4f91543c4497370cd4f8955a3d8b4012cd
stripy1026 <aequatio1026@gmail.com>
2023-11-03 14:40:18 +0900

    feat: :sparkles: place artist component into main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import { Artist } from '@/components/Artist/Artist'
 import Banner from '@/components/Banner/Banner'
 import { News } from '@/components/News/News'
@@ -16,10 +15,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <Artist />
+      <main>
         <Banner />
         <News />
+        <Artist />
       </main>
     </>
   )
