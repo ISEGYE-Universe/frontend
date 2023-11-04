2e40664109b9a4e06bc31a6634f90223b8bd9ed4
rnintai <ho46825@gmail.com>
2023-11-04 16:50:43 +0900

    fix: :bug: ellipsis character '…' (U+2026) 폰트 적용 제외
    가운데 정렬 된 말줄임표의 원인은 폰트이기 때문에 해당 글자만 적용 제외

--- src/styles/Global.tsx
+++ src/styles/Global.tsx
@@ -12,12 +12,12 @@ const style = css`
     padding: 0;
   }
 
-  h1, 
-  h2, 
-  h3, 
-  h4, 
-  h5, 
-  h6{
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
     margin: 0;
   }
 
@@ -30,42 +30,50 @@ const style = css`
     font-weight: 100;
     font-style: normal;
     src: url('/fonts/NotoSansCJKkr-Thin.otf') format('opentype');
+    // exclude ellipsis (…, U+2026)
+    unicode-range: U+0-2025, U+2027-10FFFF;
   }
   @font-face {
     font-family: 'NotoSansCJKkr';
     font-weight: 200;
     font-style: normal;
     src: url('/fonts/NotoSansCJKkr-Light.otf') format('opentype');
+    unicode-range: U+0-2025, U+2027-10FFFF;
   }
   @font-face {
     font-family: 'NotoSansCJKkr';
     font-weight: 300;
     font-style: normal;
     src: url('/fonts/NotoSansCJKkr-DemiLight.otf') format('opentype');
+    unicode-range: U+0-2025, U+2027-10FFFF;
   }
   @font-face {
     font-family: 'NotoSansCJKkr';
     font-weight: 400;
     font-style: normal;
     src: url('/fonts/NotoSansCJKkr-Regular.otf') format('opentype');
+    unicode-range: U+0-2025, U+2027-10FFFF;
   }
   @font-face {
     font-family: 'NotoSansCJKkr';
     font-weight: 500;
     font-style: normal;
     src: url('/fonts/NotoSansCJKkr-Medium.otf') format('opentype');
+    unicode-range: U+0-2025, U+2027-10FFFF;
   }
   @font-face {
     font-family: 'NotoSansCJKkr';
     font-weight: 600;
     font-style: normal;
     src: url('/fonts/NotoSansCJKkr-Bold.otf') format('opentype');
+    unicode-range: U+0-2025, U+2027-10FFFF;
   }
   @font-face {
     font-family: 'NotoSansCJKkr';
     font-weight: 700;
     font-style: normal;
     src: url('/fonts/NotoSansCJKkr-Black.otf') format('opentype');
+    unicode-range: U+0-2025, U+2027-10FFFF;
   }
 
   button {
