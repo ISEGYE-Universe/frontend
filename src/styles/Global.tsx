da15755f88e2efa109ff5eadeb918004c9aea3f5
rnintai <ho46825@gmail.com>
2023-11-07 22:04:41 +0900

    revert: :rewind: Font, Global css 관련 변경사항 revert

--- src/styles/Global.tsx
+++ src/styles/Global.tsx
@@ -12,23 +12,6 @@ const style = css`
     padding: 0;
   }
 
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    margin: 0;
-  }
-
-  a {
-    text-decoration: none;
-  }
-
-  li {
-    list-style: none;
-  }
-
   @font-face {
     font-family: 'NotoSansCJKkr';
     font-weight: 100;
