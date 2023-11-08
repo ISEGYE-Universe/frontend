1731c9c608788f085a6885f7873c6b2c6af3a7c4
spde3289 <spde3289@naver.com>
2023-11-08 11:19:16 +0900

    feat: movie 임시 데이터 변경

--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -4,20 +4,32 @@ import { MovieListType } from './type/types'
 /** 임시 데이터 */
 const MovieList: MovieListType = [
   {
-    title: '키딩',
-    src: 'https://www.youtube.com/embed/rDFUl2mHIW4?si=u-LS5UhmVzZCc9Zk',
+    title: 'The 1st Single - 리와인드 (RE:WIND)',
+    src: 'https://www.youtube.com/embed/fgSXAKsq-Vo?si=sWt4P_hUeyISCtCm',
   },
   {
-    title: '어쩌구',
+    title: 'The 2nd Single - 겨울봄',
     src: 'https://www.youtube.com/embed/JY-gJkMuJ94?si=7pXjsRq3MIn5nZC2',
   },
   {
-    title: '저쩌구',
-    src: 'https://www.youtube.com/embed/fgSXAKsq-Vo?si=RWCIxW4dIlX_n0r8',
+    title: 'OST - LOCKDOWN (락다운)',
+    src: 'https://www.youtube.com/embed/QgMFpuos4Rg?si=DXiy2xN3oPBpTPUY',
   },
   {
-    title: 'ㅇㅇㅇ',
-    src: 'https://www.youtube.com/embed/i4SRH9jfUMQ?si=RTDntVz_SxqoRjwK',
+    title: 'OST - Another World (어나더 월드)',
+    src: 'https://www.youtube.com/embed/8KTFf2X-ago?si=uA1il-de4NToZqae',
+  },
+  {
+    title: 'The 3rd Single - KIDDING (키딩)',
+    src: 'https://www.youtube.com/embed/rDFUl2mHIW4?si=N76Z7HzkjDmHT1d9',
+  },
+  {
+    title: 'ETC - OVER (오버)',
+    src: 'https://www.youtube.com/embed/i4SRH9jfUMQ?si=CCYt9Q6Ai1zjwm8w',
+  },
+  {
+    title: 'ETC - Superhero (슈퍼히어로)',
+    src: 'https://www.youtube.com/embed/wyhwJnNpVJI?si=u3XNIlKDcV5MsYYg',
   },
 ]
 
