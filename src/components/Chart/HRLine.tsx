ade1074b53b92acbd221f9d9b222bc3ad5cf3ad4
rnintai <ho46825@gmail.com>
2023-11-19 11:13:51 +0900

    fix: :rotating_light: [TEMP] react/require-default-props 주석으로 disable 처리
    react/require-default-props 룰 관련 의사결정 이후 처리 예정

--- src/components/Chart/HRLine.tsx
+++ src/components/Chart/HRLine.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable react/require-default-props */
 import { ChartColor } from '@/styles/ChartColor'
 import { css } from '@emotion/react'
 
 interface HRLineProps {
   margin?: string
 }
-const HRLine = ({ margin }: HRLineProps) => {
+const HRLine: React.FC<HRLineProps> = ({ margin }: HRLineProps) => {
   return (
     <hr
       css={css`
