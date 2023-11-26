61f6710cf33d012de4314ddf0904d1b4d965a3a9
rnintai <ho46825@gmail.com>
2023-11-26 16:44:27 +0900

    feat: :sparkles: 커버곡 영역 레이아웃 구현

--- src/components/MemberProfile/MemberProfileCoverSong.css.ts
+++ src/components/MemberProfile/MemberProfileCoverSong.css.ts
@@ -1,11 +1,142 @@
+import {
+  CaptionMdLight,
+  CaptionMdRegular,
+  LineHeight,
+  TextMdBold,
+  TextSmLight,
+  TitleSmBold,
+} from '@/styles/Font'
 import { MemberProfileColor } from '@/styles/MemberProfileColor'
 import { css } from '@emotion/react'
 
-export const mainContainer = css`
-  width: 770px;
-  height: 460px;
-  border-radius: 40px;
-  border: 2px solid ${MemberProfileColor.coverBorderGrey};
-  background: ${MemberProfileColor.coverBackgroundGrey};
-  padding: 20px 40px 20px 32px;
+const flexRow = css`
+  display: flex;
 `
+const flexCol = css`
+  display: flex;
+  flex-direction: column;
+`
+const justifySpaceBetween = css`
+  justify-content: space-between;
+`
+const alignCenter = css`
+  align-items: center;
+`
+
+export const memberProfileCoverSongMain = [
+  flexCol,
+  css`
+    width: 770px;
+    height: 460px;
+    border-radius: 40px;
+    border: 2px solid ${MemberProfileColor.coverBorderGrey};
+    background: ${MemberProfileColor.coverBackgroundGrey};
+    padding: 20px 16px 20px 32px;
+  `,
+]
+
+export const memberProfileCoverSongTitleBox = [
+  flexCol,
+  css`
+    gap: 6px;
+    border-bottom: 2px solid ${MemberProfileColor.jururuPink};
+    padding-bottom: 8px;
+    margin-right: 24px;
+    margin-bottom: 10px;
+  `,
+]
+export const memberProfileCoverSongTitleText = [
+  TitleSmBold,
+  LineHeight,
+  css`
+    color: ${MemberProfileColor.coverTitleTextBlack};
+  `,
+]
+export const memberProfileCoverSongSubTitleBox = [
+  flexRow,
+  justifySpaceBetween,
+  CaptionMdRegular,
+  LineHeight,
+  css`
+    color: ${MemberProfileColor.coverSubTitleTextGrey};
+  `,
+]
+export const memberProfileCoverSongListBox = [
+  flexCol,
+  css`
+    gap: 6px;
+    overflow-y: auto;
+    padding-right: 16px;
+  `,
+  css`
+    &::-webkit-scrollbar {
+      width: 8px;
+      background: none;
+    }
+    &::-webkit-scrollbar-thumb {
+      border-radius: 4px;
+      background: #646871;
+    }
+  `,
+]
+export const memberProfileCoverSongListItem = [
+  flexRow,
+  alignCenter,
+  css`
+    padding: 6px 24px 6px 14px;
+    border-radius: 12px;
+    transition: background-color 0.2s ease;
+    &:hover {
+      background-color: ${MemberProfileColor.jururuPinkTransparent};
+    }
+    &:hover .play-icon {
+      opacity: 1;
+    }
+  `,
+]
+export const memberProfileCoverSongListItemImage = [
+  css`
+    margin-right: 16px;
+    border-radius: 10px;
+  `,
+]
+export const memberProfileCoverSongListItemTitleBox = [
+  flexCol,
+  css`
+    gap: 10px;
+    max-width: 444px;
+    width: 100%;
+    margin-right: auto;
+  `,
+]
+export const memberProfileCoverSongListItemIndexText = [
+  CaptionMdLight,
+  LineHeight,
+  css`
+    color: ${MemberProfileColor.jururuPink};
+    margin-right: 20px;
+  `,
+]
+export const memberProfileCoverSongListItemTitleText = [
+  TextMdBold,
+  LineHeight,
+  css`
+    color: ${MemberProfileColor.coverTitleTextBlack};
+  `,
+]
+export const memberProfileCoverSongListItemDateText = [
+  TextSmLight,
+  LineHeight,
+  css`
+    color: ${MemberProfileColor.coverDateTextGrey};
+  `,
+]
+export const memberProfileCoverSongListItemPlayIcon = [
+  TextSmLight,
+  LineHeight,
+  css`
+    color: ${MemberProfileColor.coverDateTextGrey};
+    opacity: 0.5;
+    transition: opacity 0.2s ease;
+  `,
+]
--- src/components/MemberProfile/MemberProfileCoverSong.tsx
+++ src/components/MemberProfile/MemberProfileCoverSong.tsx
@@ -1,7 +1,83 @@
-import { mainContainer } from './MemberProfileCoverSong.css'
+import memberProfileData from '@/data/member-profile.json'
+import Link from 'next/link'
+import Image from 'next/image'
+import {
+  memberProfileCoverSongListBox,
+  memberProfileCoverSongListItem,
+  memberProfileCoverSongListItemDateText,
+  memberProfileCoverSongListItemIndexText,
+  memberProfileCoverSongListItemTitleText,
+  memberProfileCoverSongListItemTitleBox,
+  memberProfileCoverSongMain,
+  memberProfileCoverSongSubTitleBox,
+  memberProfileCoverSongTitleBox,
+  memberProfileCoverSongTitleText,
+  memberProfileCoverSongListItemImage,
+  memberProfileCoverSongListItemPlayIcon,
+} from './MemberProfileCoverSong.css'
 
-const MemberProfileCoverSong = () => {
-  return <section css={mainContainer} />
+interface MemberProfileCoverSongProps {
+  memberName: IsedolMember
+}
+const MemberProfileCoverSong = ({
+  memberName,
+}: MemberProfileCoverSongProps) => {
+  const { enName, groupName, krName, socialMedia } =
+    memberProfileData[memberName]?.memberInformation || {}
+  const youtubeLink = socialMedia?.youtube.url || ''
+  return (
+    <section css={memberProfileCoverSongMain}>
+      <div css={memberProfileCoverSongTitleBox}>
+        <h2 css={memberProfileCoverSongTitleText}>{enName}&apos;s Cover</h2>
+        <div css={memberProfileCoverSongSubTitleBox}>
+          <p>
+            {groupName} {krName}의 커버곡을 감상해보세요.
+          </p>
+          <Link href={youtubeLink} target="_blank">
+            More
+          </Link>
+        </div>
+      </div>
+      <ul css={memberProfileCoverSongListBox}>
+        {memberProfileData[memberName]?.recentCover.map((cover, i) => (
+          <li key={cover.id}>
+            <Link
+              key={cover.id}
+              css={memberProfileCoverSongListItem}
+              href={cover.link}
+              target="_blank"
+            >
+              <span css={memberProfileCoverSongListItemIndexText}>{i + 1}</span>
+              <Image
+                css={memberProfileCoverSongListItemImage}
+                src={cover.thumbUrl}
+                width={108}
+                height={60}
+                alt={`${cover.title} thumnail`}
+              />
+              <div css={memberProfileCoverSongListItemTitleBox}>
+                <p css={memberProfileCoverSongListItemTitleText}>
+                  {cover.title}
+                </p>
+                <p css={memberProfileCoverSongListItemDateText}>
+                  {cover.uploadDate}
+                </p>
+              </div>
+
+              <Image
+                src="/images/icon/icon-play.svg"
+                className="play-icon"
+                width={32}
+                height={32}
+                alt={`${cover.title} play icon`}
+                css={memberProfileCoverSongListItemPlayIcon}
+              />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
 }
 
 export default MemberProfileCoverSong
