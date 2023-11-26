ec283821586f4af50e57db9c4853c0de957d7637
rnintai <ho46825@gmail.com>
2023-11-26 16:44:19 +0900

    feat: :lipstick: 컬러값 정의

--- src/styles/MemberProfileColor.ts
+++ src/styles/MemberProfileColor.ts
@@ -2,4 +2,9 @@ export const MemberProfileColor = {
   coverBackgroundGrey: '#fafafa',
   coverBorderGrey: '#e3e3e3',
   wakscordBackgroundGrey: '#313338',
+  coverTitleTextBlack: '#252525',
+  coverSubTitleTextGrey: '#8d8d8d',
+  coverDateTextGrey: '#787878',
+  jururuPink: '#FF008C',
+  jururuPinkTransparent: '#FF008C1A',
 }
