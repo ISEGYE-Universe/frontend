59aa0f3ebb9e1dca4ddfbeab5e5fb80e580237e5
YoungJin.Heo <80889483+HeoYoungJin98@users.noreply.github.com>
2023-11-29 22:24:45 +0900

    :rotating_light: fix: albums - destructuring props 관련 ESLint 오류 해결 (#55)
    * Commit Test
    
    * fix: album-page props lint error
    
    * refactor: :recycle: props 선언 위치 변경
    
    * fix: prop 선언 방식 통일
    
    ---------
    
    Co-authored-by: rnintai <ho46825@gmail.com>

--- src/pages/albums/detail.tsx
+++ src/pages/albums/detail.tsx
@@ -4,26 +4,19 @@
  * Album Detail Page
  */
 import { DivMargin } from '@/styles/Album/AlbumInfo'
-import EmblaCarousel from '../../components/Albums/EmblaCarousel'
-import Album from '../../components/Albums/AlbumInfo'
+import { EmblaCarousel } from '../../components/Albums/EmblaCarousel'
 
 const Detail = () => {
   return (
     <main className="album-detail-div" css={DivMargin}>
       <EmblaCarousel
-        slides={[
-          <Album ident="REWIND" />,
-          <Album ident="WINTER_SPRING" />,
-          <Album ident="LOCKDOWN" />,
-          <Album ident="ANOTHER_WORLD" />,
-          <Album ident="KIDDING" />,
-        ]}
+        data={['REWIND', 'WINTERSPRING', 'LOCKDOWN', 'ANOTHERWORLD', 'KIDDING']}
         options={{
           align: 'start',
           loop: true,
           skipSnaps: false,
         }}
-        default={0}
+        defaultIndex={0}
       />
     </main>
   )
