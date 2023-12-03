1ba11d438228e35fc584b5bec37244be43a67ec7
rnintai <ho46825@gmail.com>
2023-12-03 21:26:30 +0900

    feat: :sparkles: 왁스코드 영역 마크업

--- src/styles/MemberProfileColor.ts
+++ src/styles/MemberProfileColor.ts
@@ -2,4 +2,6 @@ export const MemberProfileColor = {
   coverBackgroundGrey: '#fafafa',
   coverBorderGrey: '#e3e3e3',
   wakscordBackgroundGrey: '#313338',
+  wakscordButtonBackgroundGrey: '#4F5259',
+  wakscordTextGrey: '#FAFAFA',
 }
