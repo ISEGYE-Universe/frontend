8da3670bbeb0e5cb2820bb35870355a2547f1918
rnintai <ho46825@gmail.com>
2023-12-07 22:03:25 +0900

    feat: :lipstick: background position css 수정

--- src/components/MemberProfile/MemberProfileTopBanner.css.ts
+++ src/components/MemberProfile/MemberProfileTopBanner.css.ts
@@ -14,6 +14,7 @@ export const topBannerContainer = (topBannerImage: string) => {
     height: 360px;
     background-repeat: no-repeat;
     background-size: cover;
+    background-position: center;
     margin-bottom: 60px;
   `
 }
