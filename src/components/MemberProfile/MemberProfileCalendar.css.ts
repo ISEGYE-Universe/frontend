c6c944422b1e0f65e2f917bf3a1576a3587b6fb9
rnintai <ho46825@gmail.com>
2023-12-08 23:53:23 +0900

    feat: :sparkles: react-calendar 기반 스케줄 달력 마크업

--- src/components/MemberProfile/MemberProfileCalendar.css.ts
+++ src/components/MemberProfile/MemberProfileCalendar.css.ts
@@ -14,6 +14,25 @@ export const calendarStyle = [
   css`
     display: flex;
     flex-direction: column;
+    color: #fafafa;
+
+    .react-calendar__navigation {
+      display: flex;
+      gap: 20px;
+      .react-calendar__navigation__label {
+        cursor: default;
+        flex-grow: 0 !important;
+      }
+      .react-calendar__navigation__prev2-button,
+      .react-calendar__navigation__next2-button {
+        display: none;
+      }
+      .react-calendar__navigation__prev-button,
+      .react-calendar__navigation__next-button {
+        display: flex;
+        align-items: center;
+      }
+    }
 
     .react-calendar__viewContainer {
       align-self: flex-end;
@@ -22,6 +41,7 @@ export const calendarStyle = [
       .react-calendar__month-view__weekdays {
         gap: 32px;
         margin-bottom: 32px;
+        font-weight: 700;
         .react-calendar__month-view__weekdays__weekday {
           text-align: center;
           flex: 0 0 50px !important;
@@ -32,16 +52,38 @@ export const calendarStyle = [
           abbr {
             text-decoration: none;
           }
+          &.react-calendar__month-view__weekdays__weekday--weekend {
+            abbr[aria-label='토요일'] {
+              color: #2400ff;
+            }
+            abbr[aria-label='일요일'] {
+              color: #f00;
+            }
+          }
         }
       }
 
       .react-calendar__month-view__days {
         gap: 32px;
+        &:first-child() {
+        }
         .react-calendar__tile {
           flex: 0 0 50px !important;
           height: 50px;
+        }
+        .react-calendar__month-view__days__day {
+          color: #c7c7c7;
+          cursor: default;
+        }
+        .react-calendar__month-view__days__day--neighboringMonth {
+          visibility: hidden;
         }
       }
     }
   `,
 ]
+
+export const calendarNavigationMonth = css`
+  font-weight: 700;
+  margin-left: 20px;
+`
--- src/components/MemberProfile/MemberProfileCalendar.tsx
+++ src/components/MemberProfile/MemberProfileCalendar.tsx
@@ -1,16 +1,43 @@
 import Calendar from 'react-calendar'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { NavigationLabelArgs } from 'react-calendar/dist/cjs/shared/types'
+import Image from 'next/image'
 import {
   calendarContentContainer,
+  calendarNavigationMonth,
   calendarStyle,
 } from './MemberProfileCalendar.css'
+import { MemberProfileCalendarStreamList } from './MemberProfileCalendarStreamList'
 
+// 한 자리 수 앞에 leading zero 붙여주는 함수
 const addZero = (num: number) => {
   return num < 10 ? `0${num}` : num
 }
 
+// Date 객체 -> yyyy-mm으로 변경 해주는 함수
+const formatYearMonth = (date: Date) => {
+  return `${date.getFullYear()}-${addZero(date.getMonth() + 1)}`
+}
+
+// navigation label 스타일링을 위한 ReactNode를 반환
+const navigationLabelNode = (prop: NavigationLabelArgs) => {
+  const { date } = prop
+  return (
+    <>
+      <span>{date.getFullYear()}</span>
+      <span css={calendarNavigationMonth}>{addZero(date.getMonth() + 1)}</span>
+    </>
+  )
+}
 export const MemberProfileCalendar = () => {
-  const [value, setValue] = useState<Date>(new Date())
+  const [value] = useState<Date>(new Date())
+  const [currentMonth, setCurrentMonth] = useState<string>(
+    formatYearMonth(value),
+  )
+
+  useEffect(() => {
+    console.log(currentMonth)
+  }, [currentMonth])
 
   return (
     <div css={calendarContentContainer}>
@@ -23,20 +50,44 @@ export const MemberProfileCalendar = () => {
         css={calendarStyle}
         value={value}
         calendarType="gregory"
-        navigationLabel={({ date, label, locale, view }) =>
-          `${date.getFullYear()} ${addZero(date.getMonth() + 1)}`
+        // navigation 연, 월 표시 Label 커스텀
+        navigationLabel={(prop) => {
+          return navigationLabelNode(prop)
+        }}
+        // navigation 좌,우 버튼 커스텀
+        prevLabel={
+          <Image
+            src="/images/icon/member-profile/keyboard-arrow-left.svg"
+            alt="left icon"
+            width={8}
+            height={12}
+          />
         }
-        // 요일 표시 label 변경
+        nextLabel={
+          <Image
+            src="/images/icon/member-profile/keyboard-arrow-right.svg"
+            alt="right icon"
+            width={8}
+            height={12}
+          />
+        }
+        // 요일 표시 label 커스텀
         formatShortWeekday={(locale, date) =>
           ['S', 'M', 'T', 'W', 'T', 'F', 'S'][date.getDay()]
         }
-        // 일 표시 format 변경
+        // 일 표시 format 커스텀
         formatDay={(locale, date) =>
           new Intl.DateTimeFormat('en-US', {
             day: 'numeric',
           }).format(date)
         }
+        onActiveStartDateChange={({ activeStartDate }) => {
+          if (activeStartDate) {
+            setCurrentMonth(formatYearMonth(activeStartDate))
+          }
+        }}
       />
+      <MemberProfileCalendarStreamList />
     </div>
   )
 }
