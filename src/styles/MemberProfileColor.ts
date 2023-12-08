9554ac7cdd8b0a0e7f5b9d84b31e4fcb88c93c33
rnintai <ho46825@gmail.com>
2023-12-09 00:50:04 +0900

    feat: :lipstick: 컬러값 정의

--- src/styles/MemberProfileColor.ts
+++ src/styles/MemberProfileColor.ts
@@ -2,4 +2,8 @@ export const MemberProfileColor = {
   coverBackgroundGrey: '#fafafa',
   coverBorderGrey: '#e3e3e3',
   wakscordBackgroundGrey: '#313338',
+  scheduleTextRed: '#f00',
+  scheduleTextBlue: '#2400ff',
+  scheduleTextGrey: '#c7c7c7',
+  scheduleTextWhite: '#fafafa',
 }
