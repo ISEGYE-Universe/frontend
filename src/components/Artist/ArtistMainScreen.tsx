08e611064a286841ca179daa057ff3465db96dd6
stripy1026 <aequatio1026@gmail.com>
2023-12-17 01:41:53 +0900

    refactor: :art: move wallpaper data to json

--- src/components/Artist/ArtistMainScreen.tsx
+++ src/components/Artist/ArtistMainScreen.tsx
@@ -1,31 +1,6 @@
 import Image from 'next/image'
 
-const SCREEN_MAP = {
-  Ine: {
-    imgSrc: 'images/main-artist/이네땅_바탕화면_1.png',
-    color: '#8A2BE2',
-  },
-  Jingburger: {
-    imgSrc: 'images/main-artist/버거땅_바탕화면_1.png',
-    color: '#F0A957',
-  },
-  Lilpa: {
-    imgSrc: 'images/main-artist/릴파땅_바탕화면_1.png',
-    color: '#443965',
-  },
-  Jururu: {
-    imgSrc: 'images/main-artist/르르땅_바탕화면_1.png',
-    color: '#FF008C',
-  },
-  Gosegu: {
-    imgSrc: 'images/main-artist/세구땅_바탕화면_1.png',
-    color: '#467EC6',
-  },
-  VIichan: {
-    imgSrc: 'images/main-artist/챠니_바탕화면_1.png',
-    color: '#95C100',
-  },
-}
+import wallpaper from '@/data/main-artist/artist-wallpaper.json'
 
 type ArtistMainScreenProps = {
   highlightedMember: string
@@ -40,7 +15,7 @@ export const ArtistMainScreen = ({
         css={{
           borderRadius: 20,
         }}
-        src={SCREEN_MAP[highlightedMember as keyof typeof SCREEN_MAP].imgSrc}
+        src={wallpaper[highlightedMember as keyof typeof wallpaper].imgSrc}
         alt="main screen image"
         width={934}
         height={520}
