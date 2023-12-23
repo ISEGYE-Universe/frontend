f8ca9cd2dfb8148df08e9d70fd4d57159098d645
rnintai <ho46825@gmail.com>
2023-12-23 11:03:29 +0900

    feat: :sparkles: @visx/tooltip 활용 tooltip 및 annotation init

--- src/components/Chart/Top100Chart.tsx
+++ src/components/Chart/Top100Chart.tsx
@@ -4,14 +4,16 @@ import { WithParentSizeProps } from '@visx/responsive/lib/enhancers/withParentSi
 import {
   AnimatedAreaSeries,
   Axis,
+  EventHandlerParams,
   GlyphSeries,
   Grid,
   Margin,
-  Tooltip,
+  TooltipProvider,
   XYChart,
 } from '@visx/xychart'
 import { Annotation, HtmlLabel } from '@visx/annotation'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { useTooltip, useTooltipInPortal } from '@visx/tooltip'
 import {
   tooltipContainer,
   tooltipRankText,
@@ -51,7 +53,7 @@ interface Top100ChartImplProps {
   parentWidth?: number
   parentHeight?: number
   margin?: Margin
-  data?: MelonTop100Datum[]
+  data: MelonTop100Datum[]
 }
 
 interface Point {
@@ -72,148 +74,237 @@ const Top100ChartImpl = ({
   const [annotationOpen, setAnnotationOpen] = useState<boolean>(false)
   const [isPointerOut, setIsPointerOut] = useState<boolean>(false)
 
+  // tooltip
+  const containerOuterRef = useRef<HTMLDivElement>(null)
+  const [circleDOMList, setCircleDOMList] = useState<Element[]>(
+    new Array(data.length),
+  )
+
+  const {
+    tooltipData,
+    tooltipLeft,
+    tooltipTop,
+    tooltipOpen,
+    showTooltip,
+    hideTooltip,
+  } = useTooltip()
+
+  const { containerRef, TooltipInPortal } = useTooltipInPortal({
+    detectBounds: true,
+    scroll: true,
+  })
+
+  const handleMouseOver = useCallback(
+    (event: EventHandlerParams<MelonTop100Datum>) => {
+      const isLastGlyph = event.index === data.length - 1
+      if (isLastGlyph && lastGlyphPointLoaded) {
+        return null
+      }
+
+      const parentDOM = containerOuterRef.current
+      let cx: SVGAnimatedLength
+      let cy: SVGAnimatedLength
+      if (parentDOM) {
+        if (!circleDOMList[event.index]) {
+          // cache miss, store value
+          const currentGlyph = parentDOM?.querySelector(
+            `.glyph-${event.index}`,
+          ) as SVGCircleElement
+          setCircleDOMList([
+            ...circleDOMList.slice(0, event.index),
+            currentGlyph,
+            ...circleDOMList.slice(event.index + 1),
+          ])
+
+          cx = currentGlyph.cx
+          cy = currentGlyph.cy
+        } else {
+          // get DOM from cache
+          const currentGlyph = circleDOMList[event.index] as SVGCircleElement
+
+          cx = currentGlyph.cx
+          cy = currentGlyph.cy
+        }
+        if (cx && cy) {
+          showTooltip({
+            tooltipLeft: cx.baseVal.value - 50,
+            tooltipTop: cy.baseVal.value - 25,
+            tooltipData: event.datum.rank,
+          })
+        }
+      }
+      return null
+    },
+    [circleDOMList, showTooltip],
+  )
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      const currentGlyph = containerOuterRef.current?.querySelector(
+        `.glyph-${data.length - 1}`,
+      ) as SVGCircleElement
+
+      setLastGlyphPoint({
+        x: currentGlyph.cx.baseVal.value,
+        y: currentGlyph.cy.baseVal.value,
+      })
+
+      setLastGlyphPointLoaded(true)
+    }, 100)
+
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [])
+
   return (
-    <div css={top100ChartContainer}>
-      <XYChart
-        onPointerOut={() => {
-          setIsPointerOut(true)
-          setAnnotationOpen(false)
-        }}
-        onPointerMove={() => {
-          if (isPointerOut) {
-            setIsPointerOut(false)
-          }
-        }}
-        margin={margin || { top: 10, right: 0, bottom: 20, left: 0 }}
-        width={graphWidth}
-        height={graphHeight}
-        xScale={{
-          type: 'band',
-        }}
-        yScale={{
-          // logarithm 그래프 활용하여 뒤집힌 도메인 바로잡기 (음수의 log는 양수)
-          type: 'log',
-          // y축 도메인 뒤집기 (순위)
-          domain: [maxY + 3, minY - 3],
-          base: 2,
-        }}
-      >
-        {data ? (
-          <>
-            <Grid
-              numTicks={20}
-              rows={false}
-              lineStyle={{ backgroundColor: ChartColor.chartBorderGrey }}
-            />
-            <Grid
-              numTicks={6}
-              columns={false}
-              lineStyle={{ backgroundColor: ChartColor.chartBorderGrey }}
-            />
-            <Axis
-              orientation="bottom"
-              hideAxisLine
-              axisClassName="axis-line"
-              // ex) 13:00 -> 13
-              tickFormat={(d: string) => {
-                return d.slice(0, 2)
-              }}
-              tickClassName="axis-tick"
-            />
-            <AnimatedAreaSeries
-              data={data}
-              dataKey="top100LineGraph"
-              fill="rgba(234, 68, 118, 0.15)"
-              lineProps={{
-                stroke: ChartColor.isedolPink,
-                strokeWidth: 1,
-              }}
-              xAccessor={accessors.xAccessor}
-              yAccessor={accessors.yAccessor}
-            />
+    <TooltipProvider>
+      <div ref={containerOuterRef}>
+        <div ref={containerRef} css={top100ChartContainer}>
+          <XYChart
+            margin={margin || { top: 10, right: 0, bottom: 20, left: 0 }}
+            width={graphWidth}
+            height={graphHeight}
+            xScale={{
+              type: 'band',
+            }}
+            yScale={{
+              // logarithm 그래프 활용하여 뒤집힌 도메인 바로잡기 (음수의 log는 양수)
+              type: 'log',
+              // y축 도메인 뒤집기 (순위)
+              domain: [maxY + 3, minY - 3],
+              base: 2,
+            }}
+          >
+            {data ? (
+              <>
+                <Grid
+                  numTicks={20}
+                  rows={false}
+                  lineStyle={{ backgroundColor: ChartColor.chartBorderGrey }}
+                />
+                <Grid
+                  numTicks={6}
+                  columns={false}
+                  lineStyle={{ backgroundColor: ChartColor.chartBorderGrey }}
+                />
+                <Axis
+                  orientation="bottom"
+                  hideAxisLine
+                  axisClassName="axis-line"
+                  // ex) 13:00 -> 13
+                  tickFormat={(d: string) => {
+                    return d.slice(0, 2)
+                  }}
+                  tickClassName="axis-tick"
+                />
+                <AnimatedAreaSeries
+                  data={data}
+                  dataKey="top100LineGraph"
+                  fill="rgba(234, 68, 118, 0.15)"
+                  lineProps={{
+                    stroke: ChartColor.isedolPink,
+                    strokeWidth: 1,
+                  }}
+                  xAccessor={accessors.xAccessor}
+                  yAccessor={accessors.yAccessor}
+                />
 
-            {/* Glyph */}
-            <GlyphSeries
-              dataKey="top100Glyph"
-              data={data}
-              xAccessor={accessors.xAccessor}
-              yAccessor={accessors.yAccessor}
-              colorAccessor={accessors.colorAccessor}
-              // size={5}
-              renderGlyph={(datum) => {
-                // 마지막 glyph의 좌표 저장
-                if (
-                  datum.key === `${data.length - 1}` &&
-                  !lastGlyphPointLoaded
-                ) {
-                  setLastGlyphPoint({ x: datum.x, y: datum.y })
-                  setLastGlyphPointLoaded(true)
-                }
-                return (
-                  <circle
-                    cx={datum.x}
-                    cy={datum.y}
-                    r={3}
-                    fill={ChartColor.isedolPink}
-                  />
-                )
-              }}
-            />
+                {/* Glyph */}
+                <GlyphSeries
+                  dataKey="top100Glyph"
+                  data={data}
+                  xAccessor={accessors.xAccessor}
+                  yAccessor={accessors.yAccessor}
+                  colorAccessor={accessors.colorAccessor}
+                  onPointerMove={handleMouseOver}
+                  onPointerOut={hideTooltip}
+                  renderGlyph={(datum) => {
+                    return (
+                      <circle
+                        className={`visx-glyph glyph-${datum.key}`}
+                        cx={datum.x}
+                        cy={datum.y}
+                        r={3}
+                        fill={ChartColor.isedolPink}
+                      />
+                    )
+                  }}
+                />
+                {tooltipOpen && tooltipData && (
+                  <TooltipInPortal
+                    key={Math.random()}
+                    top={tooltipTop}
+                    left={tooltipLeft}
+                  >
+                    {tooltipData as string}
+                  </TooltipInPortal>
+                )}
+                {/* <Tooltip<MelonTop100Datum>
+                offsetTop={-23}
+                offsetLeft={-63}
+                renderTooltip={({ tooltipData }) => {
+                  if (
+                    tooltipData !== undefined &&
+                    tooltipData.nearestDatum &&
+                    // 가장 최근 데이터의 tooltip은 표시하지 않음
+                    tooltipData.nearestDatum?.index < data.length - 1 &&
+                    !isPointerOut
+                  ) {
+                    setAnnotationOpen(true)
 
-            <Tooltip<MelonTop100Datum>
-              offsetTop={-23}
-              offsetLeft={-63}
-              renderTooltip={({ tooltipData }) => {
-                if (
-                  tooltipData !== undefined &&
-                  tooltipData.nearestDatum &&
-                  // 가장 최근 데이터의 tooltip은 표시하지 않음
-                  tooltipData.nearestDatum?.index < data.length - 1 &&
-                  !isPointerOut
-                ) {
-                  setAnnotationOpen(true)
-
-                  return (
-                    <div
-                      css={tooltipContainer(false)}
-                      key={tooltipData.nearestDatum?.key}
-                    >
-                      <p css={tooltipTimeText}>
-                        {tooltipData.nearestDatum?.datum
-                          ? accessors.xAccessor(tooltipData.nearestDatum.datum)
-                          : null}
-                      </p>
-                      <p css={tooltipRankText}>
-                        {tooltipData.nearestDatum?.datum
-                          ? accessors.yAccessor(tooltipData.nearestDatum.datum)
-                          : null}
-                        위
-                      </p>
-                    </div>
-                  )
-                }
-                setAnnotationOpen(false)
-                return null
-              }}
-              snapTooltipToDatumX
-              snapTooltipToDatumY
-              unstyled
-              applyPositionStyle
-            />
-            {lastGlyphPointLoaded && (
-              <Annotation x={lastGlyphPoint.x - 15} y={lastGlyphPoint.y + 20}>
-                <HtmlLabel>
-                  <div css={tooltipContainer(annotationOpen)}>
-                    <p css={tooltipTimeText}>현재 순위</p>
-                    <p css={tooltipRankText}>{data[data.length - 1].rank}위!</p>
-                  </div>
-                </HtmlLabel>
-              </Annotation>
-            )}
-          </>
-        ) : null}
-      </XYChart>
-    </div>
+                    return (
+                      <div
+                        css={tooltipContainer(false)}
+                        key={tooltipData.nearestDatum?.key}
+                      >
+                        <p css={tooltipTimeText}>
+                          {tooltipData.nearestDatum?.datum
+                            ? accessors.xAccessor(
+                                tooltipData.nearestDatum.datum,
+                              )
+                            : null}
+                        </p>
+                        <p css={tooltipRankText}>
+                          {tooltipData.nearestDatum?.datum
+                            ? accessors.yAccessor(
+                                tooltipData.nearestDatum.datum,
+                              )
+                            : null}
+                          위
+                        </p>
+                      </div>
+                    )
+                  }
+                  setAnnotationOpen(false)
+                  return null
+                }}
+                snapTooltipToDatumX
+                snapTooltipToDatumY
+                unstyled
+                applyPositionStyle
+              /> */}
+                {lastGlyphPointLoaded && (
+                  <Annotation
+                    x={lastGlyphPoint.x - 15}
+                    y={lastGlyphPoint.y + 20}
+                  >
+                    <HtmlLabel>
+                      <div css={tooltipContainer(annotationOpen)}>
+                        <p css={tooltipTimeText}>현재 순위</p>
+                        <p css={tooltipRankText}>
+                          {data[data.length - 1].rank}위!
+                        </p>
+                      </div>
+                    </HtmlLabel>
+                  </Annotation>
+                )}
+              </>
+            ) : null}
+          </XYChart>
+        </div>
+      </div>
+    </TooltipProvider>
   )
 }
 
