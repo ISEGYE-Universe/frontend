e6f977f53a200731353d6e60022c2a1a0baef1e4
rnintai <ho46825@gmail.com>
2023-12-31 02:21:12 +0900

    feat: :sparkles: 위젯 마우스 오버 레이아웃 구현

--- src/components/MemberProfile/MemberProfilePlayerWidget.css.ts
+++ src/components/MemberProfile/MemberProfilePlayerWidget.css.ts
@@ -1,21 +1,50 @@
 import ChartColor from '@/styles/ChartColor'
+import { TextSmBold } from '@/styles/Font'
 import { css } from '@emotion/react'
 
-export const widgetContainer = (color: string, isReady: boolean) => css`
+export const widgetContainer = (
+  color: string,
+  isReady: boolean,
+  isHover: boolean,
+) => css`
   display: flex;
   background-color: ${ChartColor.textWhite};
   justify-content: center;
   align-items: center;
   position: sticky;
-  width: 44px;
+  min-width: 44px;
   height: 44px;
+  padding: ${isHover ? '0 12px' : '0 8px'};
   top: calc(100vh - 150px);
   border: 2px solid ${color};
-  box-shadow: 1px 1px 6px rgba(255, 108, 153, 0.7);
+  box-shadow: 1px 1px 6px ${`${color}7d`};
   border-radius: 9999px;
   z-index: 10;
 
   opacity: ${isReady ? 1 : 0.3};
   ${!isReady ? `pointer-events: none;` : ``}
-  transition: opacity 0.2s ease;
+  transition: opacity 0.2s, padding 0.2s;
 `
+
+export const widgetIconContainer = css`
+  display: flex;
+  align-items: center;
+  transition: gap 0.5s;
+`
+
+export const widgetIconWrapper = css`
+  display: flex;
+  align-items: center;
+  width: 24px;
+  height: 100%;
+`
+
+export const widgetText = (color: string) => [
+  TextSmBold,
+  css`
+    color: ${color};
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  `,
+]
--- src/components/MemberProfile/MemberProfilePlayerWidget.tsx
+++ src/components/MemberProfile/MemberProfilePlayerWidget.tsx
@@ -1,8 +1,14 @@
 import memberProfileData from '@/data/member-profile.json'
 import MemberProfileStore from '@/store/MemberProfile/MemberProfileStore'
-import { widgetContainer } from './MemberProfilePlayerWidget.css'
+import { useState } from 'react'
+import { motion } from 'framer-motion'
+import {
+  widgetContainer,
+  widgetIconContainer,
+  widgetIconWrapper,
+  widgetText,
+} from './MemberProfilePlayerWidget.css'
 import { WidgetPlayerIcon } from './WidgetPlayerIcon'
-import { defaultVolume } from './MemberProfileCoverSong'
 
 export const MemberProfilePlayerWidget = ({
   memberName,
@@ -11,26 +17,71 @@ export const MemberProfilePlayerWidget = ({
 }) => {
   const { personalColor } = memberProfileData[memberName] || {}
 
-  const { isPlaying, setIsPlaying, youTubePlayerReady, youTubeVideoPlayer } =
-    MemberProfileStore()
+  const {
+    isPlaying,
+    youTubePlayerReady,
+    youTubeVideoPlayer,
+    currentYoutubeId,
+  } = MemberProfileStore()
+
+  const [isHover, setIsHover] = useState<boolean>(false)
+
+  const handleMouseOver = () => {
+    setIsHover(true)
+  }
+  const handleMouseOut = () => {
+    setIsHover(false)
+  }
+
+  // 커버곡 리스트에서 현재 item 찾기
+  const getCurrentCover = (id: string) => {
+    return memberProfileData[memberName].recentCover.find((obj) =>
+      obj.link.includes(id),
+    )?.title
+  }
 
   return (
     <button
       type="button"
-      css={widgetContainer(personalColor, youTubePlayerReady)}
+      css={widgetContainer(personalColor, youTubePlayerReady, isHover)}
+      onMouseOver={handleMouseOver}
+      onFocus={handleMouseOver}
+      onMouseOut={handleMouseOut}
+      onBlur={handleMouseOut}
       aria-label="player-widget"
       onClick={() => {
         if (isPlaying) {
           youTubeVideoPlayer.pauseVideo()
         } else {
           youTubeVideoPlayer.playVideo()
-          // 볼륨 조정
-          youTubeVideoPlayer.setVolume(defaultVolume)
         }
-        setIsPlaying(!isPlaying)
       }}
     >
-      <WidgetPlayerIcon color={personalColor} animate={isPlaying} />
+      <div css={widgetIconContainer}>
+        <div css={widgetIconWrapper}>
+          <WidgetPlayerIcon color={personalColor} animate={isPlaying} />
+        </div>
+
+        <motion.p
+          animate={{
+            maxWidth: isHover ? 170 : 0,
+            opacity: isHover ? 1 : 0,
+            marginLeft: isHover ? 8 : 0,
+          }}
+          transition={{
+            duration: 0.5,
+            maxWidth: {
+              duration: 0.3,
+            },
+          }}
+          css={widgetText(personalColor)}
+          title={`${
+            memberProfileData[memberName].memberInformation.krName
+          } - ${getCurrentCover(currentYoutubeId)}`}
+        >
+          {getCurrentCover(currentYoutubeId)}
+        </motion.p>
+      </div>
     </button>
   )
 }
