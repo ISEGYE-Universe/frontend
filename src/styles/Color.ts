15b44d737e7eac2d57d9c448a5ff9689026e2292
rnintai <ho46825@gmail.com>
2024-01-26 23:17:32 +0900

    feat: :sparkles: 디자인시스템 컬러 정의

---
+++ src/styles/Color.ts
@@ -0,0 +1,17 @@
+const Color = {
+  isgidolIne: '#8A2BE2',
+  isgidolJingburger: '#F0A957',
+  isgidolLilpa: '#443965',
+  isgidolJururu: '#FF008C',
+  isgidolGosegu: '#467EC6',
+  isgidolViichan: '#95C100',
+  gray1: '#0F0F0E',
+  gray2: '#333131',
+  gray3: '#4C4B4A',
+  gray4: '#7F7E7D',
+  gray5: '#E1E1E1',
+  gray6: '#FAFAFA',
+  gradient: 'linear-gradient(90deg, #F1436F 0%, #FBAA9F 100%)',
+}
+
+export default Color
